fe2360d3c6b6fd14102ca6c4cb193264482ec759
Swastika Pani <swastikapani1234@gmail.com>
2025-12-26 21:30:50 +0530

    Initial commit of SpringBootApplication-3

---
+++ src/main/java/com/example/SpringBootApplication3Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplication3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplication3Application.class, args);
+	}
+
+}
